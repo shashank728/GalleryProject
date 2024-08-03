21878d57d164f079a832d4b19510769968922e0b
Adwait Patel <145041038+AdwaitPatel@users.noreply.github.com>
2024-08-04 00:44:28 +0530

    Category added

--- gallery/main/urls.py
+++ gallery/main/urls.py
@@ -3,14 +3,27 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from . import views
+
 urlpatterns = [
+
     path('',views.home,name="home"),
     path('Gallery/',views.gallery,name="gallery"),
     path('add_img/',views.add_img,name="add_img"),
-    path('delete_img/<int:imgs_id>',views.delete,name="delete"),
+    
+    path('delete_img/<int:image_id>',views.delete,name="delete"),
     path('category/',views.category,name="category"),
-
-        
-
+    path('sub_category/<str:imgs_category>',views.sub_category,name="sub_category"),
+    
+    
     
 ] + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
+
+
+
+
+
+
+
+
+
+
--- gallery/main/views.py
+++ gallery/main/views.py
@@ -2,38 +2,45 @@ from django.shortcuts import render,redirect
 
 from .models import Add_img
 
-# Create your views here.
+
+
 
 def home(request):
     return render(request,"home.html")
 
 
 # ============== photos gallery ==================
+
+
+
 def gallery(request):
 
-    img = Add_img.objects.filter(category="nature")
+    img = Add_img.objects.all()
     
     parameter = {
-        "img" : img,
-
+        "img" : img
     }
     
     return render(request,"gallery.html",parameter)
 
 
+
+
 # ============= add img =======================
+
+
 def add_img(request):
+
     if request.method == "POST":
+
         user_img = request.FILES["img"] 
         category = request.POST.get("category")
         
-
-        
         new_user = Add_img(
             img = user_img,
-            category = category
-            
+            category = category     
         )
+
         new_user.save()
         
         return redirect("gallery")
@@ -43,24 +50,45 @@ def add_img(request):
 
 # ================= Delete =========================
 
-def delete(request,imgs_id):
-    image = Add_img.objects.get(id = imgs_id)
+
+def delete(request,image_id):
+    image = Add_img.objects.get(id = image_id)
     
     image.delete()
     return redirect("gallery")
-    
-    
-# ================= category ======================
+
+
+# =========================== category ======================
+
 
 def category(request):
+    categories = Add_img.objects.values_list('category', flat=True).distinct()
+    category_images = [{'category': category, 'image': Add_img.objects.filter(category=category).first()} for category in categories]
 
-    category = Add_img.objects.all()
+    context = {
+        'category_images': category_images
+    }
+    
+    return render(request, 'category.html', context)
+
+
+
+# ============================ sub category ======================
+
+
+def sub_category(request, imgs_category):
+
+    img = Add_img.objects.filter(category = imgs_category)
 
     parameter = {
-        "category": category
+        "images" : img,
+        "category": imgs_category
     }
+    
+    return render(request,"sub_category.html",parameter)
 
-    return render(request,"category.html", parameter)
+
+
 
 
 
